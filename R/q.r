5b269c8fffec16a80ef84b3e24a8f8ca9b367ce7
JVAdams <JVAdams@users.noreply.github.com>
2014-07-06 06:17:47 -0500

    Update q.r

--- R/q.r
+++ R/q.r
@@ -3,6 +3,6 @@
 #' Terminate the current R session.
 #' @param save 	A character string indicating whether the workspace should be saved, "yes" (the default), "no", "ask" or "default".
 #' @export
-#' @seealso 	\code\link{{quit}}.
+#' @seealso 	\code{\link{quit}}.
 
 q <- function() quit(save="yes")
