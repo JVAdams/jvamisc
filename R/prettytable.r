cbc206d7720f97efbce55be275b71c02d0cca4f7
JVAdams <buai.biz@gmail.com>
2014-07-31 11:03:12 -0500

    Added prettytable function

---
+++ R/prettytable.r
@@ -0,0 +1,23 @@
+#' Prettify the Numeric Columns of a Table
+#'
+#' Prettify the numeric columns of a table for printing.
+#' @param m 		Two dimensional data frame, matrix, or table to be prettified.
+#' @param sigdig	Integer scalar, number of significant digits to be used, default 3.
+#' @return 			A data frame, matrix, or table, the same dimensions as \code{m} with the numeric columns rounded to 
+#' \code{sigdig} significant digits.
+#' @seealso			\code{\link{signif}}.
+#' @export
+#' @examples 
+#' head(mtcars)
+#' prettytable(head(mtcars), 2)
+
+prettytable <- function(m, sigdig=3) {
+	mclass <- sapply(m, class)
+	nc <- mclass %in% c("numeric", "integer")
+	if(sum(nc) > 0 ) {
+		for(i in (1:dim(m)[2])[nc]) {
+			m[, nc] <- signif(m[, nc], sigdig)
+			}
+		}
+	m
+	}
