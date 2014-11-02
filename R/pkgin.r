91f5a1fa3d8afc97753779f4cf8ce3ad595f466b
JVAdams <buai.biz@gmail.com>
2014-11-02 16:18:31 -0600

    Added new package building functions

---
+++ R/pkgin.r
@@ -0,0 +1,21 @@
+#' Install a Package
+#'
+#' Install a package from local files, load the library, and save installed package to zip archive.
+#' @param package 	A character scalar, package name.
+#' @param wd 		A character scalar, R working directory, default "C:/JVA/R/Working Directory".
+#' @param ld 		A character scalar, R library directory, default "C:/Users/jvadams/Documents/R/win-library/3.1".
+#' @param pd 		A character scalar, R package directory, default "C:/JVA/GitHub".
+#' @import			devtools roxygen2
+#' @export
+#' @seealso			\code{\link{pkgup}}
+
+pkgin <- function(package, wd="C:/JVA/R/Working Directory", ld="C:/Users/jvadams/Documents/R/win-library/3.1", pd="C:/JVA/GitHub") {
+	# install from local folder
+	setwd(pd)
+	install(package)
+	# load the package
+	setwd(wd)
+	library(package, character.only=TRUE)
+	# save installed package to zip archive
+	zip(paste0(pd, "/", package, "/", package, ".zip"), paste0(ld, "/", package))
+	}
---
+++ R/pkgup.r
@@ -0,0 +1,14 @@
+#' Document a Package
+#'
+#' Document a package from local files.
+#' @param package 	A character scalar, package name.
+#' @param dir 		A character scalar, directory where package folder is located, default "C:/JVA/GitHub/".
+#' @import			devtools
+#' @export
+#' @seealso			\code{\link{pkgin}}
+
+pkgup <- function(package, dir="C:/JVA/GitHub/") {
+	setwd(paste0(dir, package))
+	document()
+	cat("\n\nQuit and restart R\n\n")
+	}
