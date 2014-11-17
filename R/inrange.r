8884ac42717fb6fdf99e734ff2bb79118f8523b5
JVAdams <buai.biz@gmail.com>
2014-11-17 11:39:48 -0600

    new minor functions

---
+++ R/inrange.r
@@ -0,0 +1,21 @@
+#' In Range
+#'
+#' Test to see if value falls within specified range.  
+#' @param x 	A numeric vector of values to be tested.
+#' @param r 	A numeric vector of length 2 specifying the range.
+#' @param open 	A logical scalar indicating if the range interval is open (excludes endpoints, TRUE, default) or 
+#'	closed (includes endpoints, FALSE).
+#' @return 		A logical vector, the same length as \code{x} indicating if values fall within the specified range.
+#' @export
+#' @examples 
+#' inrange(4:6, c(4, 5.5))
+#' inrange(4:6, c(4, 5.5), FALSE)
+
+inrange <- function(x, r, open=TRUE) {
+	if(open) {
+		sel <- !is.na(x) & (x > r[1] & x < r[2])
+		} else {
+		sel <- !is.na(x) & x >= r[1] & x <= r[2]
+		}
+	sel
+	}
---
+++ R/trimspace.r
@@ -0,0 +1,14 @@
+#' Trim Whitespace
+#'
+#' Trim leading and trailing whitespace from a character vector.
+#' @param charvec	A character vector to be trimmed.
+#' @return 			A character vector, the same length as \code{charvec}, without leading and trailing whitespace.
+#' @export
+#' @examples 
+#' trimspace("    Bob   and Alice")
+#' trimspace(" Harriet and    June    ")
+
+trimspace <- function(charvec) {
+	gsub("(^ +)|( +$)", "", charvec)
+	}
+	
