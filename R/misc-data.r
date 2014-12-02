b0d6f32ca4a74383cd0c4f27513f081276657de4
JVAdams <buai.biz@gmail.com>
2014-12-02 13:33:05 -0600

    Added local environment to package
    Added jvamiscenv, a package local environment, in which objects can be
    referenced outside of multiple functions.  Also saved the code,
    jvamiscObjects.txt, used to create the environment as well as other
    miscellaneous data objects.

--- R/misc-data.r
+++ R/misc-data.r
@@ -3,8 +3,6 @@
 #' @description	List of five data frames, one for each Great Lake
 # '(Superior, Michigan, Huron, Erie, Ontario)
 #' with longitude and latitude coordinates of the shorelines.
-#' @docType		data
-#' @usage		mapL
 #' @format		One row for each point, consecutive rows with non-missing values for each line.
 #' @source		NOAA National Geophysical Data Center, (the now defunct) Coastline 
 #'	Extractor, \href{http://www.ngdc.noaa.gov/mgg/coast/}{ngdc.noaa.gov/mgg/coast}.
@@ -14,8 +12,6 @@ NULL
 #' @title		Great Lakes Basin Shoreline
 #' @description	A single data frame with longitude and latitude coordinates 
 #' of the shorelines in the Great Lakes basin.
-#' @docType		data
-#' @usage		map5
 #' @format		One row for each point, consecutive rows with non-missing values for each line.
 #' @source		NOAA National Geophysical Data Center, (the now defunct) Coastline 
 #'	Extractor, \href{http://www.ngdc.noaa.gov/mgg/coast/}{ngdc.noaa.gov/mgg/coast}.
@@ -24,24 +20,26 @@ NULL
 #' @name		Lakenames
 #' @title		Great Lakes Names
 #' @description	A vector with the names of the five Great Lakes.
-#' @docType		data
-#' @usage		Lakenames
 #' @format		A character vector, length 5.
 NULL
 #' @name		Lakeabbs
 #' @title		Great Lakes Abbreviations
 #' @description	A vector with the first initials of the five Great Lakes.
-#' @docType		data
-#' @usage		Lakeabbs
 #' @format		A character vector, length 5.
 NULL
 #' @name		blindcolz
 #' @title		Color-blind Friendly Colors
 #' @description	A vector with nine color-blind friendly colors in hex code.
-#' @docType		data
-#' @usage		blindcolz
 #' @format		A character vector, length 9.
 #' @source		How to make figures and presentations that are friendly to color blind people, 
 #' 20 November 2002, \href{http://jfly.iam.u-tokyo.ac.jp/html/color_blind/}{[link]}.
 #' @author		Masataka Okabe and Kei Ito.
+NULL
+#' @name		jvamiscenv
+#' @title		jvamisc package local environment
+#' @description	An environment local to the jvamisc package, used to hold objects outside of the individual package functions 
+#'	(e.g., the table and figure counts for the rtf functions).
+#' @format		An environment.
+#' @source		Post from Hadley Wickham to r-help on 2 Dec 2014 
+#'	\href{https://stat.ethz.ch/pipermail/r-help/2014-December/423847.html}{[link]}.
 NULL
