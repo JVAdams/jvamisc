d14482aea781d6a38c632ca05591be0df1a3acd0
JVAdams <buai.biz@gmail.com>
2015-01-09 15:19:47 -0600

    updated fill() function
    added argument to allow for "reset"

--- R/fill.r
+++ R/fill.r
@@ -1,19 +1,22 @@
 #' Fill in Missing Values
 #'
 #' Fill in missing values in a vector, using the last recorded value.  
-#' @param x 	A vector, can be character, numeric, or logical.
-#' @return 		A vector the same length as \code{x}, with all NAs or ""s replace by the last value for the vector.
+#' @param x 		A vector, can be character, numeric, or logical.
+#' @param resetwhen	A logical vector, the same length as \code{x}.
+#' @return 			A vector the same length as \code{x}, with all NAs or ""s replace by the last value for the vector.
 #' Note that and missing values at the beginning of the vector will not be replaced.
 #' @export
-#' @details		Similar to \code{\link[zoo]{na.locf}} in the \code{zoo} package, but works for "" in character vectors as well.
+#' @details			Similar to \code{\link[zoo]{na.locf}} in the \code{zoo} package, but works for "" in character vectors as well.
 #' @examples 
 #' numvec <- c(NA, 1:5, NA, NA, NA, 10:12, NA)
+#' group <- rep(1:3, c(4, 4, 5))
 #' fill(numvec)
+#' fill(numvec, first(group))
 #'
 #' charvec <- c("", letters[1:5], "", "", "", letters[10:12], "")
 #' fill(charvec)
 
-fill <- function(x)
+fill <- function(x, resetwhen=rep(FALSE, length(x)))
 	{
 	# fill in a vector of values
 	# assign to every NA or "" the last value for the vector
@@ -21,10 +24,10 @@ fill <- function(x)
     last <- x[1]
     if(is.character(x)){
         for(i in 2:length(x))
-            if(x[i]=="" | is.na(x[i])) y[i] <- last else last <- x[i]
+            if((x[i]=="" | is.na(x[i])) & !resetwhen[i]) y[i] <- last else last <- x[i]
         } else {
         for(i in 2:length(x))
-            if(is.na(x[i])) y[i] <- last else last <- x[i]
+            if(is.na(x[i]) & !resetwhen[i]) y[i] <- last else last <- x[i]
         }
     y
 	}
