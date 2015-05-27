5f2dd393d2c412e2a71d578a9a49ebfa35134ff8
J. V. Adams <buai.biz@gmail.com>
2015-05-27 15:10:50 -0500

    simplified jvaFirst

--- R/jvaFirst.R
+++ R/jvaFirst.R
@@ -10,9 +10,6 @@
 #' @param cont
 #'   Character scalar, prompt used for lines which continue past
 #'   first command line, default \code{"... "}.
-#' @param pkgs
-#'   Character vector, packages to be loaded, default c("rJava", "XLConnect",
-#'   "maps", "mapproj", "RColorBrewer", "mgcv", "jvamisc").
 #' @param mirror
 #'   Character scalar, CRAN mirror, default
 #'   "http://streaming.stat.iastate.edu/CRAN/".
@@ -20,21 +17,14 @@
 #'   Character scalar, type of help, default "html".
 #' @param fac
 #'   Logical scalar, use factors rather than character strings, default FALSE.
-#' @param noplots
-#'   Logical scalar, remove "saved" plots, e.g., from past runs of
-#'   \code{\link[jvamisc]{plotdf}}, default TRUE.
-#' @param show
-#'   Logical scalar, list objects in current environment, default TRUE.
 #' @export
 #' @seealso
 #'   \code{\link{options}}, \code{\link{help}}.
 #' @examples
 #' jvaFirst()
 jvaFirst <- function(maxp=500, ndec=10, cont="... ",
-	  pkgs=c("rJava", "XLConnect", "maps", "mapproj", "RColorBrewer", "mgcv",
-      "MASS", "jvamisc"),
 	  mirror="http://streaming.stat.iastate.edu/CRAN/",
-	  helpt="html", fac=FALSE, noplots=TRUE, show=TRUE) {
+	  helpt="html", fac=FALSE) {
 
 	# don't print more than maxp rows
 	options(max.print=maxp)
@@ -47,13 +37,6 @@ jvaFirst <- function(maxp=500, ndec=10, cont="... ",
   #   http://www.onthelambda.com/2014/09/17/fun-with-rprofile-and-customizing-r-startup/
 	options(continue=cont)
 
-	# attach packages
-	sshhh <- function(a.package) {
-    suppressWarnings(suppressPackageStartupMessages(
-      library(a.package, character.only=TRUE)))
-	}
-	sapply(pkgs, sshhh)
-
 	# set CRAN mirror
 	repo <- getOption("repos")
 	repo["CRAN"] <- mirror
@@ -64,15 +47,4 @@ jvaFirst <- function(maxp=500, ndec=10, cont="... ",
 
 	# prefer characters rather than factors
 	options(stringsAsFactors=fac)
-
-	# get rid of any "saved" plots, e.g., from past runs of dfplot()
-	if (noplots) {
-    .SavedPlots <- NULL
-	}
-
-	# my personal functions
-	if (show) {
-		already <- ls(".GlobalEnv")
-		print(already)
-	}
 }
