127b28a4d6585f0a51b511f5e5947a93cd0956e2
J. V. Adams <buai.biz@gmail.com>
2016-08-25 13:30:12 -0500

    version 0.4.1.9000 minor changes/patches

--- R/pkgin.R
+++ R/pkgin.R
@@ -5,28 +5,19 @@
 #' @param package
 #'   A character scalar, package name.
 #' @param ld
-#'   A character scalar, R library directory,
-#'   default "C:/Users/jvadams/Documents/R/win-library/3.2".
-#' @param lv
-#'   A character scalar, R library directory version number,
-#'   default "max", which uses the latest version in \code{ld}.
+#'   A character scalar, current local directory, default \code{getwd()}.
 #' @param pd
 #'   A character scalar, R package directory, default "C:/JVA/GitHub".
 #' @export
 #' @seealso
 #'   \code{\link{pkgup}}
 
-pkgin <- function(package, ld="C:/Users/jvadams/Documents/R/win-library",
-  lv="max", pd="C:/JVA/GitHub") {
+pkgin <- function(package, ld=getwd(), pd="C:/JVA/GitHub") {
   if (!requireNamespace("devtools", quietly=TRUE)) {
     stop("devtools must be installed.", call.=FALSE)
   }
   # install from local folder
   setwd(pd)
   devtools::install(package)
-  # set the library folder as the working directory
-  if (lv=="max") {
-    lv <- max(list.files(ld))
-  }
-  setwd(paste(ld, lv, sep="/"))
+  setwd(ld)
 }
